1dc002294e0904e7a88ccadcb48f65c2bccfb58a
johnathanwastaken <danielortiz2001@hotmail.com>
2022-07-31 14:07:04 -0700

    setting up navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {} from 'react-bootstrap'
+
+export default class navbar extends Component {
+  render() {
+    return (
+      <div>navbar</div>
+    )
+  }
+}
